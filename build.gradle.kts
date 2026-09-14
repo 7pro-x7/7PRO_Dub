fdf1f74c7355c1b674b1848b8e56f01cb35dbb91
7pro-x7 <7pro7777@gmail.com>
2026-09-15 00:15:21 +0300

    Initial commit from Pack2Git

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Plugins على مستوى المشروع
 plugins {
     id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    // مكوّل Compose (نفس إصدار Kotlin)
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    // مكوّل Compose (نفس إصدار Kotlin) - هذا البلوجن متوفر فقط بدءًا من Kotlin 2.0+
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
